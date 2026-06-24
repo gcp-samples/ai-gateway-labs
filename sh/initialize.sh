e702ea2cf511032954ab768d976bbd34cb7c8f30
tyayers <tyler.ayers@gmail.com>
2026-06-24 08:34:05 +0200

    Update initialize.sh

--- sh/initialize.sh
+++ sh/initialize.sh
@@ -20,16 +20,21 @@ if [ -z "$APIGEE_TYPE" ]; then
   APIGEE_TYPE=EVALUATION
 fi
 
+if [ -z "$UNIQUE_NAME" ]; then
+  UNIQUE_NAME=$USER
+fi
+
 read -e -i "$GOOGLE_CLOUD_PROJECT" -p "Enter your Google Cloud Project Id: " project_id
 read -e -i "$GOOGLE_CLOUD_LOCATION" -p "Enter your Google Cloud Region: " region
 read -e -i "$APIGEE_TYPE" -p "Enter your Apigee deployment type (EVALUATION, PAYG, SUBSCRIPTION): " apigee_type
+read -e -i "$UNIQUE_NAME" -p "Enter a unique prefix for your assets: " UNIQUE_NAME
 
 echo "export GOOGLE_CLOUD_PROJECT=$project_id" > .env
 echo "export GOOGLE_CLOUD_LOCATION=$region" >> .env
 echo "export APIGEE_TYPE=$apigee_type" >> .env
 echo >> .env;
 echo "# Optional Variables" >> .env;
-echo "export UNIQUE_NAME=\$USER" >> .env
+echo "export UNIQUE_NAME=\$UNIQUE_NAME" >> .env
 echo "export APIGEE_VPC_NAME=" >> .env
 echo "export APIGEE_SUBNET_NAME=" >> .env
 echo "export APIGEE_DRZ_LOCATION=" >> .env
